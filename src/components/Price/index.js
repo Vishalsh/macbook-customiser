055504daa3bf8343ca81de5bfc1a4f910beeaba8
Vishal Sharma <sharvishi9118@gmail.com>
2021-07-11 09:46:55 +0530

    displaying the default price without any customisation

--- src/components/Price/index.js
+++ src/components/Price/index.js
@@ -1,13 +1,46 @@
 import React from "react";
+import { getDefaultPrice } from "../../service";
 
 import './index.css';
 
+const initialState = {
+  loading: false,
+  price: null,
+  error: null
+}
+
 class Price extends React.Component {
+  state = initialState;
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    getDefaultPrice()
+      .then((data) => {
+        this.setState({ price: data.value })
+      })
+      .catch((error) => {
+        this.setState(error)
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      })
+  }
+
   render() {
+    const { loading, price } = this.state;
+
     return (
       <div className="price container">
         <div className="price__content">
-          <h1 className="price__value">Total: </h1>
+          <h1 className="price__value">
+            Total:
+            {
+              loading ?
+                <>...loading</>
+                :
+                <>{` ₹${price}`}</>
+            }
+          </h1>
         </div>
       </div>
     )
